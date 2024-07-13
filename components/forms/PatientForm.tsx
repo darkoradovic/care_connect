b5a78d775b84f70a88507b8fd70739f7ea302556
darkoradovic <darmior5@gmail.com>
2024-07-13 10:22:25 +0200

    feat: onsumit register extract user data

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -27,7 +27,22 @@ export const PatientForm = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
-    console.log(values);
+    setIsLoading(true);
+    try {
+      const user = {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      };
+
+      /*    const newUser = await createUser(user);
+
+      if (newUser) {
+        router.push(`/patients/${newUser.$id}/register`);
+      } */
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
